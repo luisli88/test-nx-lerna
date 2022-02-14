4eb4ca971d9e09eacc8b92a95915095f31fe2bf4
Luis Ricardo Ruiz <luis.ruiz@mercadolibre.com.co>
2022-02-14 13:01:32 -0500

    feat(class name): added the custom class names to the parameters of the Pkg component
    BREAKING CHANGE: Added a new parameter into the Pkg Props

--- packages/pkg/src/lib/pkg.tsx
+++ packages/pkg/src/lib/pkg.tsx
@@ -4,13 +4,14 @@ import './pkg.module.scss';
 export interface PkgProps {
   name: string
   title: string
+  customStyles?: string
 }
 
 export function Pkg(props: PkgProps) {
-  const { name, title } = props;
+  const { name, title, customStyles = '' } = props;
   return (
     <div className='pkg__container'>
-      <h1 className='pkg__title'>Welcome to {title}, {name}!</h1>
+      <h1 className={`pkg__title ${customStyles}`}>Welcome to {title}, {name}!</h1>
     </div>
   );
 }
